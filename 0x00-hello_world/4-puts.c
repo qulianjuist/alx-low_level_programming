f9527403c6672fc85e108ab1b7938adda3183be3
Julian Quist <info.julianquist@gmail.com>
2023-07-13 15:14:37 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using puts function to write a c program that prints a line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
