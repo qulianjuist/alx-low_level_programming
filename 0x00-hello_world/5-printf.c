4243533ee340a604ca0c4214a2b745b5ba18df61
Julian Quist <info.julianquist@gmail.com>
2023-07-13 15:29:48 -0700

    second c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using printf function to write a c program that prints a line
+ * Return: 0
+ */
+int main(void)
+{
+print("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
