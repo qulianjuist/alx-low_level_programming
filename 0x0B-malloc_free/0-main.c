088621200235f1bd674b21c8a62004301c6db771
Julian Quist <info.julianquist@gmail.com>
2023-08-09 15:05:15 -0700

    done

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -37,12 +37,12 @@ int main(void)
 {
 1char *buffer;
 buffer = create_array(98, 'H');
-if  (buffer == NULL)
+if (buffer == NULL)
 {
 printf("failed to allocate memory\n");
- return (1);
+return (1);
 }
 simple_print_buffer(buffer, 98);
- free(buffer);
- return (0);
+free(buffer);
+return (0);
 }
